f86d5b483771bbdada8d4960aea322391f90eb3a
AnnaTopchii <113939011+AnnaTopchii@users.noreply.github.com>
2023-03-21 13:53:46 +0200

    fix

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -8,12 +8,12 @@ import PropTypes from 'prop-types';
 
 export const Section = ({ title, children }) => (
   <>
-    <h1>{title}</h1>
-    <div>{children}</div>
+    {title && <h1>{title}</h1>}
+    {children}
   </>
 );
 
 Section.propTypes = {
   good: PropTypes.string,
-  children: PropTypes.string,
+  children: PropTypes.node,
 };
